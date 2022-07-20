3fdec4112e64f15e1df6264de53cbc16f470c7c4
Vincent2466 <99465696+Vincent2466@users.noreply.github.com>
2022-07-20 12:35:40 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 class Hello
 {
-    public static void main(String [] args)
-    {
-        System.out.println("Hello world");
-    }
+    public static void main(String [] args)
+    {
+        System.out.println("Hello world");
+    }
 }
 
